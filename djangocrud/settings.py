3fe45c218edd51fc3b67c27442750d5839a56013
SamRuaF <samruafontalvo@gmail.com>
2024-01-15 12:02:07 -0500

    puse el puerto

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -86,8 +86,10 @@ DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
         default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+        conn_max_age=600,
+        
+    ),
+    'PORT': '5432',
 }
 
 
