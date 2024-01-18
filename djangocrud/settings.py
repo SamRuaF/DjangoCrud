c190828a91c0bb6534d05dbbc86efd93b21aea74
SamRuaF <samruafontalvo@gmail.com>
2024-01-18 00:46:30 -0500

    staticfiles_dirs

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -14,19 +14,17 @@ from pathlib import Path
 import os
 
 
-TEMPLATES = [
-   {
-       
-       'APP_DIRS': True,
-       
-   },
-]
+
 
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 
 # Quick-start development settings - unsuitable for production
